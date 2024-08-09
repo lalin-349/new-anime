6c1381f4ee8fc20220db7ab304ee1967f8137256
lalin-349 <lalinsekhar2003@gmail.com>
2024-08-09 12:41:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var slider1 = new Swiper ('.slider1', {
+    spaceBetween: 30,
+    loop: true,
+    effect: "fade",
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 1600,
+    }
+  });
+
+
+
+    var slider2 = new Swiper(".slider2", {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+      loop: true,
+      pagination: {
+        el: ".modal-swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+            nextEl: '.modal-swiper-button-next',
+            prevEl: '.modal-swiper-button-prev',
+      },
+
+      breakpoints: {
+        0: {
+          slidesPerView: 2
+        },
+        620: {
+          slidesPerView: 3
+        },
+
+        738: {
+          slidesPerView: 3
+        },
+
+        830: {
+          slidesPerView: 4
+        },
+
+        // 940: {
+        //   slidesPerView: 4
+        // },
+
+        1024: {
+          slidesPerView: 5
+        },
+         
+        1229: {
+          slidesPerView: 6
+        }
+      }
+    });
+
+    const dots = document.querySelector(".dots");
+    dots.onclick = function() {
+    const navphone =document.querySelector(".nav_phone")
+  .navphone.classList.toggle("active");
+}
+
+
+
