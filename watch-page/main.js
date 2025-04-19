5dc2d9678a45af3bfc097a14cfa045281a90d4ca
lalin-349 <lalinsekhar2003@gmail.com>
2025-04-19 13:37:33 +0530

    update infinity

--- watch-page/main.js
+++ watch-page/main.js
@@ -8,10 +8,37 @@ const playOne = () =>{
 document.querySelector(".btn1").addEventListener("click",()=> playOne());
 
 const playTwo = () =>{
+    const gg = document.querySelector(".btn2");
+    const gb = document.querySelector(".video-tag");
+    gb.src = gg.getAttribute("src");
+    gb.load();
+    gb.play();
+}
+document.querySelector(".btn2").addEventListener("click",()=> playTwo());
+
+const playThree = () =>{
     const gg = document.querySelector(".btn3");
     const gb = document.querySelector(".video-tag");
     gb.src = gg.getAttribute("src");
     gb.load();
     gb.play();
 }
-document.querySelector(".btn3").addEventListener("click",()=> playTwo());
+document.querySelector(".btn3").addEventListener("click",()=> playThree());
+
+const playFour = () =>{
+    const gg = document.querySelector(".btn4");
+    const gb = document.querySelector(".video-tag");
+    gb.src = gg.getAttribute("src");
+    gb.load();
+    gb.play();
+}
+document.querySelector(".btn4").addEventListener("click",()=> playFour());
+
+const playFive = () =>{
+    const gg = document.querySelector(".btn5");
+    const gb = document.querySelector(".video-tag");
+    gb.src = gg.getAttribute("src");
+    gb.load();
+    gb.play();
+}
+document.querySelector(".btn5").addEventListener("click",()=> playFive());
