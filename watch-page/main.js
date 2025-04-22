81fa468e3304e29e51c1100c9196ca17bf6b4639
lalin-349 <lalinsekhar2003@gmail.com>
2025-04-22 14:39:28 +0530

    updateOne

--- watch-page/main.js
+++ watch-page/main.js
@@ -1,3 +1,4 @@
+
 const playOne = () =>{
     const gg = document.querySelector(".btn1");
     const gb = document.querySelector(".video-tag");
@@ -9,7 +10,10 @@ const playOne = () =>{
     const description = "The One Piece anime, adapted from Eiichiro Oda's manga, is a legendary tale of adventure, friendship, and dreams. It follows Monkey D. Luffy and his crew, the Straw Hat Pirates, as they sail the Grand Line in search of the mythical treasure known as the One Piece, aiming to make Luffy the King of the Pirates. Renowned for its world-building, humor, and emotional depth, the anime has been airing since 1999 and boasts over 1,000 episodes. Its captivating arcs and unforgettable characters have made it a cornerstone of the anime world.";
     const des = document.querySelector(".des").textContent = description;
 }
-document.querySelector(".btn1").addEventListener("click",()=> playOne());
+document.querySelector(".btn1 img").addEventListener("click",()=>{ playOne();
+    window.scrollBy(0,-1000);});
+document.querySelector(".p1 p").addEventListener("click",()=>{ playOne();
+    window.scrollBy(0,-1000);});
 
 const playTwo = () =>{
     const gg = document.querySelector(".btn2");
@@ -22,7 +26,10 @@ const playTwo = () =>{
     const description = "The **Naruto** anime, based on Masashi Kishimoto's manga, is a beloved tale of perseverance, friendship, and self-discovery. It follows Naruto Uzumaki, a young ninja ostracized by his village for being the host of the Nine-Tails Fox, a powerful beast sealed within him. Despite this, Naruto dreams of becoming the Hokage, the leader of his village, to earn the respect and recognition of his peers."
     const des = document.querySelector(".des").textContent = description;
  }
-document.querySelector(".btn2").addEventListener("click",()=> playTwo());
+document.querySelector(".btn2 img").addEventListener("click",()=> { playTwo();
+    window.scrollBy(0,-1000);});
+document.querySelector(".p2 p").addEventListener("click",()=> { playTwo();
+    window.scrollBy(0,-1000);});
 
 const playThree = () =>{
     const gg = document.querySelector(".btn3");
@@ -35,7 +42,10 @@ const playThree = () =>{
     const description = "Jujutsu Kaisen is a thrilling anime and manga series that dives into a world of curses and sorcery. The story follows Yuji Itadori, a high school student who accidentally becomes the host of a powerful curse named Sukuna after swallowing a cursed object. Guided by skilled sorcerers like Satoru Gojo, Yuji trains at Tokyo Jujutsu High to battle curses and protect humanity. The series is celebrated for its intense battles, emotional depth, and stunning animation.";
     const des = document.querySelector(".des").textContent = description;
 }
-document.querySelector(".btn3").addEventListener("click",()=> playThree());
+document.querySelector(".btn3 img").addEventListener("click",()=> { playThree();
+    window.scrollBy(0,-1000);});
+document.querySelector(".p3 p").addEventListener("click",()=> { playThree();
+    window.scrollBy(0,-1000);});
 
 const playFour = () =>{
     const gg = document.querySelector(".btn4");
@@ -48,7 +58,10 @@ const playFour = () =>{
     const description = "Demon Slayer: Kimetsu no Yaiba is a captivating anime and manga series set in Taisho-era Japan. It follows Tanjiro Kamado, a kind-hearted boy who becomes a demon slayer after his family is tragically killed by demons, and his sister Nezuko is transformed into one. Tanjiro embarks on a journey to avenge his family and find a cure for Nezuko, encountering fierce battles, breathtaking animation, and a rich cast of characters along the way";
     const des = document.querySelector(".des").textContent = description;
 }
-document.querySelector(".btn4").addEventListener("click",()=> playFour());
+document.querySelector(".btn4 img").addEventListener("click",()=> { playFour();
+    window.scrollBy(0,-1000);});
+document.querySelector(".p4 p").addEventListener("click",()=> { playFour();
+    window.scrollBy(0,-1000);});
 
 const playFive = () =>{
     const gg = document.querySelector(".btn5");
@@ -59,4 +72,104 @@ const playFive = () =>{
     const pic = document.querySelector(".banner-image").src = "/img2/tomo.jpg";
     const title = document.querySelector(".title").textContent = "A edit";
 }
-document.querySelector(".btn5").addEventListener("click",()=> playFive());
+document.querySelector(".btn5 img").addEventListener("click",()=> { playFive();
+    window.scrollBy(0,-10000);});
+document.querySelector(".p5 p").addEventListener("click",()=> { playFive();
+    window.scrollBy(0,-10000);});
+
+    const playSix = () =>{
+        const gg = document.querySelector(".btn6");
+        const gb = document.querySelector(".video-tag");
+        gb.src = gg.getAttribute("src");
+        gb.load();
+        gb.play();
+        const pic = document.querySelector(".banner-image").src = "/img2/wind.jpg";
+        const title = document.querySelector(".title").textContent = "Wind Breaker";
+        const description = "The anime Wind Breaker follows the story of Haruka Sakura, a skilled fighter who enrolls at Furin High School, a place known for valuing strength over academics. Initially focused on climbing to the top, Haruka discovers the school's true mission: protecting their town from harm. Along the way, he learns the value of teamwork and fighting for others, rather than just himself. It's an action-packed series with themes of camaraderie and personal growth.";
+        const des = document.querySelector(".des").textContent = description;
+    }
+    document.querySelector(".btn6 img").addEventListener("click",()=> { playSix();
+        window.scrollBy(0,-1000);});
+    document.querySelector(".p6 p").addEventListener("click",()=> { playSix();
+        window.scrollBy(0,-1000);});
+    
+    const playSeven = () =>{
+        const gg = document.querySelector(".btn7");
+        const gb = document.querySelector(".video-tag");
+        gb.src = gg.getAttribute("src");
+        gb.load();
+        gb.play();
+        const pic = document.querySelector(".banner-image").src = "/img2/classe.jpg";
+        const title = document.querySelector(".title").textContent = "Classroom of the elite";
+        const description = "**Classroom of the Elite** is a psychological thriller anime based on the light novel series by Shōgo Kinugasa. The story revolves around Kiyotaka Ayanokoji, a seemingly ordinary student at Tokyo Koudo Ikusei Senior High School, a prestigious institution where students are ranked based on merit. Kiyotaka finds himself in Class 1-D, which is considered the lowest tier, but he soon reveals his exceptional intellect and strategic thinking as he navigates the school's competitive and manipulative environment.";
+        const des = document.querySelector(".des").textContent = description;
+    }
+    document.querySelector(".btn7 img").addEventListener("click",()=> { playSeven();
+        window.scrollBy(0,-1000);});
+    document.querySelector(".p7 p").addEventListener("click",()=> { playSeven();
+        window.scrollBy(0,-1000);});
+
+    const playEight = () =>{
+        const gg = document.querySelector(".btn8");
+        const gb = document.querySelector(".video-tag");
+        gb.src = gg.getAttribute("src");
+        gb.load();
+        gb.play();
+        const pic = document.querySelector(".banner-image").src = "/img2/summer.jpg";
+        const title = document.querySelector(".title").textContent = "summertime render";
+        const description = "**Summertime Render** (or *Summer Time Rendering*) is a gripping mystery and supernatural anime. The story follows Shinpei Ajiro, who returns to his hometown on Hitogashima Island for the funeral of his childhood friend, Ushio. However, strange occurrences and the discovery of mysterious entities called Shadows lead him to uncover dark secrets about the island. With time loops and suspenseful twists, the anime keeps viewers on the edge of their seats.";
+        const des = document.querySelector(".des").textContent = description;
+    }
+    document.querySelector(".btn8 img").addEventListener("click",()=> { playEight();
+        window.scrollBy(0,-1000);});
+
+    document.querySelector(".p8 p").addEventListener("click",()=> { playEight();
+        window.scrollBy(0,-1000);});
+    
+        const playNine = () =>{
+            const gg = document.querySelector(".btn9");
+            const gb = document.querySelector(".video-tag");
+            gb.src = gg.getAttribute("src");
+            gb.load();
+            gb.play();
+            const pic = document.querySelector(".banner-image").src = "/img2/undead.jpg";
+            const title = document.querySelector(".title").textContent = "Undead unlock";
+            const description = "";
+            const des = document.querySelector(".des").textContent = description;
+        }
+        document.querySelector(".btn9 img").addEventListener("click",()=> { playNine();
+            window.scrollBy(0,-1000);});
+        document.querySelector(".p9 p").addEventListener("click",()=> { playNine();
+            window.scrollBy(0,-1000);});
+
+        const playTen= () =>{
+            const gg = document.querySelector(".btn10");
+            const gb = document.querySelector(".video-tag");
+            gb.src = gg.getAttribute("src");
+            gb.load();
+            gb.play();
+            const pic = document.querySelector(".banner-image").src = "/img2/dark.jpg";
+            const title = document.querySelector(".title").textContent = "Dark gathering";
+            const description = "";
+            const des = document.querySelector(".des").textContent = description;
+        }
+        document.querySelector(".btn10 img").addEventListener("click",()=> { playTen();
+            window.scrollBy(0,-1000);});
+        document.querySelector(".p10 p").addEventListener("click",()=> { playTen();
+            window.scrollBy(0,-1000);});
+
+        const playEleven= () =>{
+            const gg = document.querySelector(".btn11");
+            const gb = document.querySelector(".video-tag");
+            gb.src = gg.getAttribute("src");
+            gb.load();
+            gb.play();
+            const pic = document.querySelector(".banner-image").src = "/img2/mill.jpg";
+            const title = document.querySelector(".title").textContent = "The millionaire detective";
+            const description = "";
+            const des = document.querySelector(".des").textContent = description;
+        }
+        document.querySelector(".btn11 img").addEventListener("click",()=> { playEleven();
+            window.scrollBy(0,-1000);});
+        document.querySelector(".p11 p").addEventListener("click",()=> { playEleven();
+            window.scrollBy(0,-1000);});
