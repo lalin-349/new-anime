aec7f298c254c54c0eb374dff40a04f6a395336f
lalin-349 <lalinsekhar2003@gmail.com>
2025-05-07 13:01:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,17 +101,36 @@ var slider1 = new Swiper ('.slider1', {
     }
 });
   document.addEventListener("DOMContentLoaded", () => {
-    let wordNumbers = ["one", "two", "three", "four", "five", "six", "seven", 
-                       "eight", "nine", "ten", "eleven", "twelve", "thirteen", 
-                       "fourteen", "fifteen", "sixteen", "seventeen"];
+    let wordNumbers = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+    ];
 
     for (let i = 1; i <= 16; i++) {
-        let picElement = document.getElementById(`slide${wordNumbers[i - 1]}`); // Match picOne, picTwo, etc.
+      let picElement = document.getElementById(`slide${wordNumbers[i - 1]}`); // Match picOne, picTwo, etc.
 
-        if (picElement) { // Check if the element exists before adding the event listener
-            picElement.addEventListener("click", () => {
-                window.location.href = `watch-page/index.html?slide=${wordNumbers[i - 1]}`;
-            });
-        }
+      if (picElement) {
+        // Check if the element exists before adding the event listener
+        picElement.addEventListener("click", () => {
+          window.location.href = `watch-page/index.html?slide=${
+            wordNumbers[i - 1]
+          }`;
+        });
+      }
     }
-});
+  });
